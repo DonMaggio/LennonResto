db99aafadc79b018c2eeab829b73236facaac05a
Vale <valeriasosabrandariz@gmail.com>
2024-10-01 20:12:16 -0300

    se completó de JS de la funcionalidad de los botones para cambiar el status de los pedidos dentro de las vistas de pedidos pendientes y cumplidos

--- lennonapp/static/js/orders.js
+++ lennonapp/static/js/orders.js
@@ -1,18 +1,40 @@
-const overlay = document.getElementById('overlay');
-const detallesContenido = document.getElementById('detalles-contenido');
+const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('value');
 
-function showTab(tabName) {
-    const tabs = document.querySelectorAll('.tab-content');
-    tabs.forEach(tab => {
-        tab.classList.remove('active');
-    });
-    document.getElementById(tabName).classList.add('active');
+document.addEventListener("DOMContentLoaded", function() {
+    // Selecciona todos los botones "Completado"
+    const buttons = document.querySelectorAll('.btn-ver-detalles');
 
-    const buttons = document.querySelectorAll('.tab-button');
     buttons.forEach(button => {
-        button.classList.remove('active');
+        button.addEventListener('click', function() {
+            const pedidoId = this.getAttribute('data-id');
+            const currentStatus = this.getAttribute('data-status') === 'True'; // Suponiendo que tienes un atributo data-status
+
+            // Cambia el estado
+            const newStatus = !currentStatus;
+
+            // Realiza la solicitud PATCH
+            fetch(`/orders/change/${pedidoId}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-CSRFToken': csrfToken // Agrega el token CSRF
+                },
+                body: JSON.stringify({ status: newStatus }) // Cuerpo de la solicitud en formato JSON
+            })
+            .then(response => {
+                if (response.redirected) {
+                    window.location.href = response.url;
+                } else {
+                    return response.json();
+                }
+            })
+            .then(data => {
+                console.log('Pedido actualizado:', data);
+                // Actualiza el atributo data-status del botón
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+        });
     });
-    document.querySelector(`.tab-button[onclick="showTab('${tabName}')"]`).classList.add('active');
-}
-
-
+});
